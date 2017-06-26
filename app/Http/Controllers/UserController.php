33adec552fb056dcd07fb078c192675290b45d72
Hoang Nguyen <hnguyen0428@gmail.com>
2017-06-26 15:14:15 -0700

    Clean up user search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Response;
 use App\User;
-use App\Relationship;
 use App\Http\Traits\SearchUserTrait;
 
 /**
@@ -96,35 +95,7 @@ class UserController extends Controller
         $api_token = $request->header('Authorization');
         $user_id = User::where('api_token', $api_token)->first()->id;
 
-        /* Gets all the user id where names are similar */
-        $similar_names_id = $this->similarTo($user_id, $name)->toArray();
-
-        /* Gets all the user id where users are friends of logged in user */
-        $friends_id = $this->getAllFriendsId($user_id)->toArray();
-
-        /* Gets all the user id where users and mutual friends of logged
-        in user */
-        $mutual_friends_id = $this->getAllMutualFriendsId($user_id)->toArray();
-
-        /* Retains only friends'id that have names similar to the name
-        searched */
-        $friends_id = array_filter($friends_id, function ($id)
-        use ($similar_names_id) {
-            return in_array($id, $similar_names_id);
-        });
-
-        /* Retains only mutual friends'id that have names similar to the name
-         searched */
-        $mutual_friends_id = array_filter($mutual_friends_id, function ($id)
-        use ($similar_names_id) {
-            return in_array($id, $similar_names_id);
-        });
-
-        /* Sorts the array in the order friends_id, mutual_friends_id, all
-        the rest */
-        $result_id = array_merge($friends_id, $mutual_friends_id);
-        $rest_similar_names = array_diff($similar_names_id, $result_id);
-        $result_id = array_merge($result_id, $rest_similar_names);
+        $result_id = $this->searchUserId($user_id, $name);
 
         $result_id_string = implode(",", $result_id);
         $searchResult = User::select('*')
--- app/Http/Traits/SearchUserTrait.php
+++ app/Http/Traits/SearchUserTrait.php
@@ -41,17 +41,7 @@ trait SearchUserTrait
      */
     public function getAllMutualFriendsId($user_id)
     {
-        $friends1 = Relationship::select('requestee as friend_id')
-            ->where([
-                ['requester', '=', $user_id],
-                ['status', '=', true]
-            ]);
-        $friends2 = Relationship::select('requester as friend_id')
-            ->where([
-                ['requestee', '=', $user_id],
-                ['status', '=', true]
-            ]);
-        $friends_id = $friends1->union($friends2)->get();
+        $friends_id = $this->getAllFriendsId($user_id);
 
         $mutualFriends1 = Relationship::select('requestee as user_id')
             ->whereIn('requester', $friends_id);
@@ -76,6 +66,47 @@ trait SearchUserTrait
             ['users.name', 'like', '%' . $name . '%'],
             ['users.id', '!=', $user_id]
         ])->get()->pluck('id');
+    }
+
+    /**
+     * Returns an array containing the users'id that resulted from the search
+     *
+     * @param $user_id , person searching
+     * @param $name , name to search
+     * @return array , array containing result users'id
+     */
+    public function searchUserId($user_id, $name)
+    {
+        /* Gets all the user id where names are similar */
+        $similar_names_id = $this->similarTo($user_id, $name)->toArray();
+
+        /* Gets all the user id where users are friends of logged in user */
+        $friends_id = $this->getAllFriendsId($user_id)->toArray();
+
+        /* Gets all the user id where users and mutual friends of logged
+        in user */
+        $mutual_friends_id = $this->getAllMutualFriendsId($user_id)->toArray();
+
+        /* Retains only friends'id that have names similar to the name
+        searched */
+        $friends_id = array_filter($friends_id, function ($id)
+        use ($similar_names_id) {
+            return in_array($id, $similar_names_id);
+        });
+
+        /* Retains only mutual friends'id that have names similar to the name
+         searched */
+        $mutual_friends_id = array_filter($mutual_friends_id, function ($id)
+        use ($similar_names_id) {
+            return in_array($id, $similar_names_id);
+        });
+
+        /* Sorts the array in the order friends_id, mutual_friends_id, all
+        the rest */
+        $result_id = array_merge($friends_id, $mutual_friends_id);
+        $rest_similar_names = array_diff($similar_names_id, $result_id);
+        $result_id = array_merge($result_id, $rest_similar_names);
+        return $result_id;
     }
 
 }
