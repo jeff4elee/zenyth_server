6ac0dabb7213d91fe44f5025867e2f6c4fb412d4
Hoang Nguyen <hnguyen0428@gmail.com>
2017-07-12 22:48:09 -0700

    new blade

--- resources/views/restore_password_web.blade.php
+++ resources/views/restore_password_web.blade.php
@@ -1,31 +1,55 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-@section('title')
-Zenyth Reset Password
-@endsection
+    <title>Reset Password</title>
 
-@section('content')
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
 
-<script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+
+        <h2 class="form-signin-heading">Reset your password</h2>
+        <label for="inputEmail" class="sr-only">Password</label>
+        <input type="password" id="password" class="form-control" placeholder="Password" required autofocus>
+        <label for="inputPassword" class="sr-only">Confirm Password</label>
+        <input type="password" id="password_confirmation" class="form-control" placeholder="Confirm Password" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit" id="Submit">Sign in</button>
+
+</div> <!-- /container -->
+
+
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="{{ URL::to('src/js/resetpassword.js') }}">
     var token = '{{ Session::token() }}';
     var url = '{{ route('api_pw_reset', ['token' => $token]) }}';
 </script>
-
-<div class="row">
-    <div class="col-md-4 col-md-offset-4" id="body">
-        <h3>Reset Password</h3>
-            <div class="form-group">
-                <label for="password">New Password</label>
-                <input class="form-control" type="password" name="password" id="password">
-            </div>
-
-            <div class="form-group">
-                <label for="password">Confirm New Password</label>
-                <input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
-            </div>
-            <button type="submit" class="btn-primary" id="Submit">Submit</button>
-    </div>
-</div>
-
-
-@endsection
+</body>
+</html>
