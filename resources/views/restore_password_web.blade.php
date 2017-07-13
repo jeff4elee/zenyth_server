ff8d06c12eb4658073def9b2e61fd2f7ae5a4b34
Hoang Nguyen <hnguyen0428@gmail.com>
2017-07-13 00:00:56 -0700

    stylesheet

--- resources/views/restore_password_web.blade.php
+++ resources/views/restore_password_web.blade.php
@@ -22,17 +22,18 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="{{ URL::to('src/js/resetpassword.js') }}">
-        var token = '{{ Session::token() }}';
-        var url = '{{ route('api_pw_reset', ['token' => $token]) }}';
-        console.log(url);
 
-    </script>
     <![endif]-->
 </head>
 
 <body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="{{ URL::to('src/js/resetpassword.js') }}">
+    var token = '{{ Session::token() }}';
+    var url = '{{ route('api_pw_reset', ['token' => $token]) }}';
+    console.log(url);
+</script>
 
 <div class="container">
 
