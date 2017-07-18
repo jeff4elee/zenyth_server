37dd9eef8ba00e1a654c13e4f9b21b9c5d729b97
Hoang Nguyen <hnguyen0428@yahoo.com>
2017-07-18 02:57:00 -0700

    timestamps false

--- app/Oauth.php
+++ app/Oauth.php
@@ -9,6 +9,8 @@ class Oauth extends Model
     protected $table = "oauths";
     public $timestamps = false;
 
+    protected $fillable = ['user_id', 'facebook', 'google'];
+
     public function user()
     {
         return $this->belongsTo('App\User', 'user_id');
