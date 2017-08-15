f1f4a57cc2f0f91629d1ee644f8afd0c1d5af59b
Hoang Nguyen <hnguyen0428@yahoo.com>
2017-08-14 17:42:44 -0700

    move client migration files to a subfolder

---
+++ database/migrations/clients/2017_08_14_233726_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->string('client_id');
+            $table->string('product_name');
+            $table->string('platform');
+            $table->timestamp('created_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
