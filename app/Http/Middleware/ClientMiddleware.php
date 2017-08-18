74b71ef0818a4eaac8bb5740d356ec58d9dc3f57
Hoang Nguyen <hnguyen0428@yahoo.com>
2017-08-18 02:20:27 -0700

    fixed

--- app/Http/Middleware/ClientMiddleware.php
+++ app/Http/Middleware/ClientMiddleware.php
@@ -21,7 +21,7 @@ class ClientMiddleware
     public function handle(Request $request, Closure $next)
     {
         $clientId = $request->header('Client-ID');
-        
+
         if($clientId) {
             $clients = $this->clientRepo->all();
             foreach($clients as $client) {
