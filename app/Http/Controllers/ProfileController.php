923ce901bbc5939bd8f635600a996aa32c07a883
Hoang Nguyen <hnguyen0428@yahoo.com>
2017-08-26 01:25:02 -0700

    null check

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -67,6 +67,9 @@ class ProfileController extends Controller
         }
 
         $userBeingRead = $this->userRepo->read($user_id);
+        if (!$userBeingRead)
+            Exceptions::invalidParameterException(sprintf(NOT_FOUND, USER));
+
         // Create an array to be constructed based on privacy settings
         $userPrivacy = $this->userPrivacyRepo->findBy('user_id', $user_id);
 
