4902f6c528f89e3bd83f7df98fd617e6327d83c7
Jeffrey <jeff4elee@gmail.com>
2017-09-02 12:24:57 -0700

    cache test

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ Route::post('password/reset/{token}',
     'Auth\ForgotPasswordController@restorePassword')->name('api_pw_reset');
 
 Route::group(['middleware' => 'throttle:500,1'], function() {
-    Route::get('image/{filename}', 'ImageController@showImage');
+    Route::group(['middleware' => ['caching']], function () {
+        Route::get('image/{filename}', 'ImageController@showImage');
+    });
 });
 
 Route::group(['middleware' => ['client_authorization', 'throttle:60,1']], function () {
@@ -17,9 +19,6 @@ Route::group(['middleware' => ['client_authorization', 'throttle:60,1']], functi
 
     Route::get('/profile/profile_picture/{user_id}', 'ProfileController@showProfileImage');
 
-    Route::group(['middleware' => ['caching']], function () {
-
-    });
 
     Route::group(['middleware' => 'validation'], function () {
         Route::group(['middleware' => 'authenticated'], function () {
